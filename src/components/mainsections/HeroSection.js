49e944962b92ddab2322c8a131839b847ccd1f3d
chrisscotto79 <55671747+chrisscotto79@users.noreply.github.com>
2023-05-01 15:42:11 -0400

    Update HeroSection.js

--- src/components/mainsections/HeroSection.js
+++ src/components/mainsections/HeroSection.js
@@ -13,7 +13,7 @@ function HeroSection() {
 
         {/* Left side picture of myself */}
         <div className="section-item1">
-          <img src={process.env.PUBLIC_URL + '/images/color_cutout.png'} alt="Its me" /> 
+          <img src={process.env.PUBLIC_URL + '/images/ColorCutout.png'} alt="Its me" /> 
         </div>
         {/* Welcome message */}
         <div className="section-item2">
